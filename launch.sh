995e45639f7ceeb4f16b44a111dedd8bbe81ef0c
Dronne34 <ozy23as@gmail.com>
2023-01-05 20:39:31 +0200

    launch modified

--- launch.sh
+++ launch.sh
@@ -11,4 +11,4 @@ echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar top 2>&1 | tee -a /tmp/polybar1.log & disown
 #polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
 
-echo "Bars launched..."
+echo "Bars launched......"
